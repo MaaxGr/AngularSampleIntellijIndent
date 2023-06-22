8db0ed600f36ca1fc6e06e25449ecf4bca47ca8e
magr <max.grossmann@guettlerlogistik.de>
2023-06-22 10:39:17 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <h1>Test</h1>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Test';
 }
